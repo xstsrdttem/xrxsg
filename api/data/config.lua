c8b3f58f73955bcb4466e922d58ecc3021f64432
xstsrdttem <37184475+xstsrdttem@users.noreply.github.com>
2018-03-08 20:29:26 +0330

    Add files via upload

---
+++ api/data/config.lua
@@ -0,0 +1,16 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore"
+  },
+  moderation = {
+    data = "/home/XrXsG/XrXsG/cli/data/moderation.json"
+  },
+  sudo_users = {
+    205549111,
+    205549111,
+  }
+}
+return _
+end
