151e900ffa415086d80fcebc0a7ab939935614c6
xstsrdttem <37184475+xstsrdttem@users.noreply.github.com>
2018-03-08 18:20:36 +0330

    Add files via upload

---
+++ api/data/config.lua
@@ -0,0 +1,16 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore"
+  },
+  moderation = {
+    data = "/home/tiiger/TiiGeR/cli/data/moderation.json"
+  },
+  sudo_users = {
+    205549111,
+    205549111,
+  }
+}
+return _
+end
