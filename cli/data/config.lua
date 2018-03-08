055eb9361f41dd1682e213f3aa4b4bd0a372a7a5
xstsrdttem <37184475+xstsrdttem@users.noreply.github.com>
2018-03-08 18:30:42 +0330

    Add files via upload

---
+++ cli/data/config.lua
@@ -0,0 +1,38 @@
+do local _ = {
+  admins = {
+    {
+      205549111,
+      "@Kiana1378"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore",
+    "Msg",
+    "BanHammer",
+    "Administrative",
+    "Help",
+    "Limitmember",
+    "Rank",
+    "Openchat",
+    "Del-Chat",
+    "Monshi",
+    "Rank",
+    "Warn",
+    "Auto-Lock",
+    "Plugins",
+    "cleanmembers",
+    "Zarinpal",
+    "Fun",
+    "addkick"
+  },
+  moderation = {
+    data = "./data/moderation.json",
+    tabgood = "./data/tabgood.json"
+  },
+  sudo_users = {
+    205549111,
+  }
+}
+return _
+end
